19c7d51a79d320baf6c5c26b0ffcbe4b5972a134
Alexander Mancevice <amancevice@cargometrics.com>
2016-10-04 13:39:27 -0400

    Added Cython

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@
 pandas_0_16=0.16.2
 pandas_0_17=0.17.1
 pandas_0_18=0.18.1
+pandas_0_19=0.19.0
 
 # 0.16
 dir=0.16
@@ -22,3 +23,9 @@ dir=0.18
 erb pandas=$pandas_0_18 Dockerfile-jupyter.erb > $dir/jupyter/Dockerfile
 erb pandas=$pandas_0_18 Dockerfile-python2.erb > $dir/python2/Dockerfile
 erb pandas=$pandas_0_18 Dockerfile-python3.erb > $dir/python3/Dockerfile
+
+# 0.19
+dir=0.19
+erb pandas=$pandas_0_19 Dockerfile-jupyter.erb > $dir/jupyter/Dockerfile
+erb pandas=$pandas_0_19 Dockerfile-python2.erb > $dir/python2/Dockerfile
+erb pandas=$pandas_0_19 Dockerfile-python3.erb > $dir/python3/Dockerfile
