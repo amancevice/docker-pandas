1f7bed5916af7ff1bbf66a68355a5799bcafc504
Alexander Mancevice <amancevice@cargometrics.com>
2017-01-11 10:20:41 -0500

    0.19.2

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ for pd in 0.19; do
   done
 done
 
-docker tag amancevice/pandas:0.19-python2 amancevice/pandas:0.19.1-python2
-docker tag amancevice/pandas:0.19-python2 amancevice/pandas:0.19.1
+docker tag amancevice/pandas:0.19-python2 amancevice/pandas:0.19.2-python2
+docker tag amancevice/pandas:0.19-python2 amancevice/pandas:0.19.2
 docker tag amancevice/pandas:0.19-python2 amancevice/pandas:0.19
 docker build -t amancevice/pandas:$pd-jupyter $pd/jupyter
-docker tag amancevice/pandas:0.19-jupyter amancevice/pandas:0.19.1-jupyter
-docker tag amancevice/pandas:0.19-python3 amancevice/pandas:0.19.1-python3
+docker tag amancevice/pandas:0.19-jupyter amancevice/pandas:0.19.2-jupyter
+docker tag amancevice/pandas:0.19-python3 amancevice/pandas:0.19.2-python3
 
 # docker tag amancevice/pandas:0.18-python2 amancevice/pandas:0.18.1-python2
 # docker tag amancevice/pandas:0.18-python2 amancevice/pandas:0.18.1
