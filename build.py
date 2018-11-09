cd561d5ea153c474729c9a75db219e1bf3428382
Alexander Mancevice <amancevice@cargometrics.com>
2018-11-09 13:02:35 -0500

    Remove support for Python 2

--- build.py
+++ build.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Build Docker Images.
 """
@@ -6,71 +5,38 @@ import os
 
 import click
 import compose.cli.command
+import docker
 
-PANDAS_VERSION = '0.21.0'
-PROJECT = compose.cli.command.get_project('.')
+DOCKER = docker.APIClient()
+LATEST_VERSION = '0.23.4'
 REPO = 'amancevice/pandas'
 
 
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
-@click.option('-a', '--alpine', help='Build on alpine', is_flag=True)
-@click.option('-d', '--debian', help='Build on debian', is_flag=True)
-@click.option('-j', '--jupyter', help='Build jupyter', is_flag=True)
-@click.option('-s', '--slim', help='Build on debian:slim', is_flag=True)
-@click.argument('VERSION', is_eager=True, nargs=-1)
-def build(alpine, debian, jupyter, slim, version):
+@click.option('-v', '--version', help='Pandas version', multiple=True)
+def build(version):
     """ Build Docker Images. """
-    versions = sorted(version)
-    builds = ['latest'] * debian + \
-             ['alpine'] * alpine + \
-             ['jupyter'] * jupyter + \
-             ['slim'] * slim
     tags = []
-    for build in builds:
-        # Get service
-        svc = PROJECT.get_service(build)
+    for pandas_version in version:
+        env = {'PANDAS_VERSION': pandas_version}
+        project = compose.cli.command.get_project('.', environment=env)
+        project.build(build_args=env)
+        vtags = [x.image_name for x in project.services]
+        tags.extend(vtags)
 
-        # Build Python3 images
-        for vsn in versions:
-            tags += build_version(build, svc, vsn)
-
-        if build != 'jupyter':
-            # Build Python2 images
-            svc.options['build']['context'] = \
-                svc.options['build']['context'].replace('Python3', 'Python2')
-            for vsn in version:
-                tags += build_version(build, svc, vsn, 'python2')
+        if pandas_version == LATEST_VERSION:
+            for tag in vtags:
+                if '-' in tag:
+                    newtag = tag.replace(f'{LATEST_VERSION}-', '')
+                else:
+                    newtag = tag.replace(LATEST_VERSION, 'latest')
+                DOCKER.tag(tag, newtag)
+                tags.append(newtag)
 
     click.echo()
-    for tag in sorted(tags):
-        click.echo(click.style(tag, fg='green'))
-
-
-def build_version(base, svc, vsn, python='python3'):
-    if base == 'jupyter':
-        tags = ["{repo}:{base}"]
-    elif base == 'latest' and python == 'python3' and vsn == PANDAS_VERSION:
-        tags = ["{repo}:{base}", "{repo}:{vsn}", "{repo}:{vsn}-{python}"]
-    elif base == 'latest' and python == 'python3':
-        tags = ["{repo}:{vsn}", "{repo}:{vsn}-{python}"]
-    elif base == 'latest' and python != 'python3':
-        tags = ["{repo}:{vsn}-{python}"]
-    elif python == 'python3' and vsn == PANDAS_VERSION:
-        tags = ["{repo}:{base}",
-                "{repo}:{vsn}-{base}",
-                "{repo}:{vsn}-{python}-{base}"]
-    elif python == 'python3':
-        tags = ["{repo}:{vsn}-{python}-{base}"]
-    else:
-        tags = ["{repo}:{vsn}-{python}-{base}"]
-    tags = sorted(x.format(repo=REPO, base=base, vsn=vsn, python=python)
-                  for x in tags)
     for tag in tags:
-        tag = tag.format(repo=REPO, base=base, vsn=vsn, python=python)
-        click.echo(click.style(tag, fg='yellow'))
-        svc.options['image'] = tag
-        svc.build(build_args_override={'PANDAS_VERSION': vsn})
-    return tags
+        click.echo(click.style(tag, fg='green'))
+    click.echo()
 
 
 if __name__ == '__main__':
